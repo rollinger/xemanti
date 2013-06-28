3a9c636d26d543189158a6fd34914e7f9e1cf815
Philipp Rollinger <philipp.rollinger@gmail.com>
2013-06-28 10:51:35 +0200

    BugFix urls.py and url in template: don not call the view as string rather than as variable.

--- xemanti/urls.py
+++ xemanti/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('xemanti.views',
     #
     
     # Home View
-    url(r'^$', 'home_view', name='home'),
+    url(r'^', 'home_view', name='home'),
     # Frequently Asked Questions
     url(r'^faq/', 'faq_view', name='faq'),
     # Impressum
@@ -28,8 +28,9 @@ urlpatterns = patterns('xemanti.views',
     #
     # Application URL Includes
     #
+    # TODO: $-sign might be wrong
     url(r'^de/$', include('ngramengine.urls')),
-    url(r'^rating/$', include('rating.urls'))
+    url(r'^rating/$', include('rating.urls')),
     #url(r'^reporting/', include('reporting.urls')),
     
     #
