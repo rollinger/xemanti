7e71363bf926131eb2429bdf5b00b0bc4338b226
rollinger <philipp.rollinger@gmail.com>
2013-07-25 21:38:05 +0200

    Add correct broker url for celery

--- xemanti/settings.py
+++ xemanti/settings.py
@@ -203,7 +203,7 @@ LOGGING = {
 import djcelery
 djcelery.setup_loader()
 # Using RabbitMQ 
-BROKER_URL = 'amqp://guest@127.0.0.1:5672//'
+BROKER_URL = "amqp://guest:guest@localhost:5672//"
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_TASK_RESULT_EXPIRES = 18000
 #CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
