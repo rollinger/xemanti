8e5157b0afe20896d6aec9ff48fe769a4d4757c0
rollinger <philipp.rollinger@gmail.com>
2013-07-27 11:00:19 +0200

    celery update

--- xemanti/settings.py
+++ xemanti/settings.py
@@ -203,11 +203,18 @@ LOGGING = {
 import djcelery
 djcelery.setup_loader()
 # Using RabbitMQ 
-BROKER_URL = "amqp://guest:guest@localhost:5672//"
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_USER = "xemanti-sudo"
+BROKER_PASSWORD = "comMahakalaxemanti."
+BROKER_VHOST = "localhost"
+# List of modules to import when celery starts.
+CELERY_IMPORTS = ("ngramengine.tasks", )
+#BROKER_URL = "amqp://guest:guest@localhost:5672//"
 #CELERY_RESULT_BACKEND = "amqp"
 #CELERY_TASK_RESULT_EXPIRES = 18000
-CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
-CELERYBEAT_LOADER = "djcelery.loaders.DjangoLoader"
+#CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
+#CELERYBEAT_LOADER = "djcelery.loaders.DjangoLoader"
 
 #
 # XEMANTIC CONFIGURATION SETTINGS
