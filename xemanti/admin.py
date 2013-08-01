55a18c215f42e1220c2f70ed8743ea88d82d49c1
rollinger <philipp.rollinger@gmail.com>
2013-08-01 23:05:57 +0200

    Removed django seo, due to encoding problems

--- xemanti/admin.py
+++ xemanti/admin.py
@@ -1,6 +1,1 @@
 #-*- coding: utf-8 -*-
-from rollyourown.seo.admin import register_seo_admin
-from django.contrib import admin
-from xemanti.seo import SiteMetadata
-
-register_seo_admin(admin.site, SiteMetadata)
