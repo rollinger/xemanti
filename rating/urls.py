d50257bda13664fc0c036b18e3cdb3f51f9705cd
rollinger <philipp.rollinger@gmail.com>
2013-08-07 21:25:47 +0200

    Bugfix urls

--- rating/urls.py
+++ rating/urls.py
@@ -7,10 +7,13 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('rating.views',
     
     # Rating View
-    url(r'^$', 'rate_assoc_view', name='rate_assoc'),
-    url(r'^(?P<ngram>[\w\ -_]+)/$', 'rate_assoc_view', name='rate_assoc'),
-    url(r'^sort/(?P<ngram>[\w\ -_]+)/$', 'sort_ngram_view', name='sort_ngram'),
+    
+    
+    
     url(r'^eval/sd/(?P<ngram>[\w\ -_]+)/$', 'eval_sem_diff_view', name='eval_sem_diff'),
+    url(r'^sort/(?P<ngram>[\w\ -_]+)/$', 'sort_ngram_view', name='sort_ngram'),
+    url(r'^(?P<ngram>[\w\ -_]+)/$', 'rate_assoc_view', name='rate_assoc'),
+    url(r'^$', 'rate_assoc_view', name='rate_assoc'),
     
     # DEPRECATED: NGram Setup Rating view for Admin
     #url(r'^ngramsetup/', 'ngram_setup_view', name='ngram_setup'),
