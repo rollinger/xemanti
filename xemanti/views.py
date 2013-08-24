5c6bbf81e6808ba87c2adb847db18863603cb0fd
rollinger <philipp.rollinger@gmail.com>
2013-08-24 10:10:28 +0200

    Add more messages

--- xemanti/views.py
+++ xemanti/views.py
@@ -55,6 +55,7 @@ def login_view(request):
             messages.add_message(request, messages.ERROR, _('Something went wrong! Username or Password did not validate.'), fail_silently=True)
             return HttpResponseRedirect("login_view")
     else:
+        messages.add_message(request, messages.INFO, _('Fill in your Credentials and log into Xemanti!'), fail_silently=True)
         return render(request, "registration/login.html", {})
     
 def logout_view(request):
@@ -71,9 +72,11 @@ def registration_view(request):
             new_user = form.save()
             new_profile = Profile(user=new_user, balance=settings.REGISTRATION_START_BALANCE)
             new_profile.save()
+            messages.add_message(request, messages.SUCCESS, _('Thanks for registering on Xemanti!'), fail_silently=True)
             return HttpResponseRedirect(reverse('login_view'))
     else:
         form = UserCreationForm()
+        messages.add_message(request, messages.INFO, _('Please fill in your registration form.'), fail_silently=True)
     return render(request, "registration/register.html", {
         'form': form,
     })
