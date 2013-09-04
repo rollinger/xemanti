de91f5472333e19dfb688b5ccfe1ba67ed6f95d8
rollinger <philipp.rollinger@gmail.com>
2013-09-04 22:37:49 +0200

    Finalized Tag Cloud

--- reporting/templatetags/reporting_tags.py
+++ reporting/templatetags/reporting_tags.py
@@ -12,8 +12,8 @@ def power_to_font_size(power):
 
 @register.inclusion_tag('reporting/_tag_cloud_partial.html')
 def render_tag_cloud(ngram, type):
-    visible = ngram.association_outbound.filter(power__gte=0.01).order_by('-power')
-    invisible = ngram.association_outbound.filter(power__lte=0.01).order_by('-power')
+    visible = getattr(ngram, type).filter(power__gte=0.01).order_by('-power')
+    invisible = getattr(ngram, type).filter(power__lte=0.01).order_by('-power')
     return {
         'ngram': ngram,
         'type':type,
