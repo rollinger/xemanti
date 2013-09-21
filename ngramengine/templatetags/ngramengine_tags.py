3540f47db9466528f3ddacac193c8fec159f63ef
rollinger <philipp.rollinger@gmail.com>
2013-09-21 16:27:54 +0200

    Update popular ngrams

--- ngramengine/templatetags/ngramengine_tags.py
+++ ngramengine/templatetags/ngramengine_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.inclusion_tag('ngramengine/_popular_ngrams_partial.html')
-def render_popular_ngrams(daterange=1, number=15):
+def render_popular_ngrams(daterange=1, number=5):
     popular_ngrams = NGrams.get_popular_ngram(daterange,number)
     return {
         'popular_ngrams': popular_ngrams,
