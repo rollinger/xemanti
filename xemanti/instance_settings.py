62d2b382ded65901cc62a8d221fe70cde315f9dd
root <root@h2170983.stratoserver.net>
2013-09-22 13:10:02 +0200

    untracked instance_settings.py

--- xemanti/instance_settings.py
+++
@@ -1,5 +0,0 @@
-# Settings for the individual instance (Development od Productive)
-# is git ignored - the file will not be transfered
-# modifications have to be edited on the server
-
-DEBUG = False
