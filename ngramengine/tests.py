c4262eb575643c738894c0cab354e77d44352161
rollinger <philipp.rollinger@gmail.com>
2013-10-26 15:24:18 +0200

    Set up functional test file

--- ngramengine/tests.py
+++ ngramengine/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -8,7 +9,299 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
+
+class NGramsFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the NGram Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class AssociationsFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the Associations Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class CoOccurrencesFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the CoOccurrences Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class NotRelatedFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the NotRelated Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class SuperCategoryFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the SuperCategory Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class SubCategoryFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the SubCategory Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class SynonymsFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the Synonyms Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class AntonymsFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the Antonyms Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class SemanticDifferentialFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the SemanticDifferential Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class SensualDimensionsFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the SensualDimensions Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class LanguagesFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the Languages Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class PartofSpeechFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the PartofSpeech Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class NumerusFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the Numerus Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class GenusFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the Genus Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+
+class WordStemsFunctionalTest(TestCase):
+    """
+    Functional Test Suite for the WordStems Model
+    """
+    def setUp(self):
+        # Routine called BEFORE each test method
+        pass
+    
+    def tearDown(self):
+        # Routine called AFTER each test method
+        pass
+    
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
