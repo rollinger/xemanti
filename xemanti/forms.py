3dd1e92811a60613bdea8808f71d7b351dd8e737
rollinger <philipp.rollinger@gmail.com>
2013-11-07 10:44:02 +0100

    Changed reporting design

--- xemanti/forms.py
+++ xemanti/forms.py
@@ -6,8 +6,6 @@ from django.utils.translation import ugettext as _
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from captcha.fields import CaptchaField
-
 
 
 class CustomUserCreationForm(UserCreationForm):
